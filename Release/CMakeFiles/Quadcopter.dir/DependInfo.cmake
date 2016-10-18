6e9dfe8b4538a4762656fea4247e5051a05ff869
mudusun <mudusun@gmail.com>
2016-10-18 12:46:52 +0200

    Add files via upload

---
+++ Release/CMakeFiles/Quadcopter.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/application/fboinsgrenderer.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/fboinsgrenderer.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/application/gmlibwrapper.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/gmlibwrapper.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/application/guiapplication.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/guiapplication.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/application/main.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/main.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/application/moc_fboinsgrenderer.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/moc_fboinsgrenderer.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/application/moc_gmlibwrapper.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/moc_gmlibwrapper.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/application/moc_guiapplication.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/moc_guiapplication.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/application/moc_window.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/moc_window.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/application/window.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/application/window.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/defaulthidmanager.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/defaulthidmanager.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidaction.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidaction.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidbinding.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidbinding.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidinput.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidinput.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidinputevent.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidinputevent.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidkbmouseinput.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidkbmouseinput.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidmanager.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidmanager.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/hidmanagertreemodel.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/hidmanagertreemodel.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/hidmanager/moc_defaulthidmanager.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/moc_defaulthidmanager.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/hidmanager/moc_hidaction.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/moc_hidaction.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/hidmanager/moc_hidmanager.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/moc_hidmanager.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/hidmanager/moc_hidmanagertreemodel.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/moc_hidmanagertreemodel.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/hidmanager/moc_standardhidmanager.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/moc_standardhidmanager.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/hidmanager/standardhidmanager.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/hidmanager/standardhidmanager.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/moc_scenario.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/moc_scenario.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/qrc_application.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/qrc_application.cpp.obj"
+  "C:/Users/hxu001/Documents/gmlib_qmldemo.git/scenario.cpp" "C:/Users/hxu001/Documents/gmlib_qmldemo.git/Release/CMakeFiles/Quadcopter.dir/scenario.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Qt/5.7/mingw53_32/include"
+  "C:/Qt/5.7/mingw53_32/include/QtCore"
+  "C:/Qt/5.7/mingw53_32/./mkspecs/win32-g++"
+  "C:/Qt/5.7/mingw53_32/include/QtQuick"
+  "C:/Qt/5.7/mingw53_32/include/QtQml"
+  "C:/Qt/5.7/mingw53_32/include/QtNetwork"
+  "C:/Qt/5.7/mingw53_32/include/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
